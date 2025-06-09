2b5750e3a6e4432165cd4590a85b0b7f824b28ed
220FasyaTriNugroho <fasya.tri.ft24@mail.umy.ac.id>
2025-06-09 18:58:23 +0700

    membuat array, kondisi catch, try

--- ReadandWrite/exceptionStandard.cpp
+++ ReadandWrite/exceptionStandard.cpp
@@ -5,5 +5,22 @@ using namespace std;
 
 int main()
 {
+    cout << "Awal Program" << endl; //penanda I
+    try
+    {
+        array<int, 3> data = {10, 20, 30};
+        //pesan array integer 3 elemen
+        cout << data.at(4) << endl;
+        //memanggil array elemen ke 5
+    }
+    catch(exception& e)
+    {
+        //penangkap menggunakan obyek exception
+        cout << e.what() << endl;
+        //akan dieksekusi karna array data hanya memiliki 3 elemen
+    }
+    cout << "Baris Program yang berakhir" << endl;
+
+    return 0;
     
 }
