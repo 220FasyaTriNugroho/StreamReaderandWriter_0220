1fa4d493b09b0172e453fffabc1754c11286436e
220FasyaTriNugroho <fasya.tri.ft24@mail.umy.ac.id>
2025-06-09 18:22:07 +0700

    membuat outfile untuk menujuk ke seluruh file

--- ReadandWrite/ioFileWithPath.cpp
+++ ReadandWrite/ioFileWithPath.cpp
@@ -13,4 +13,8 @@ int main ()
 
     //membuka file dalam mode menulis.
     ofstream outfile;
+    //menunjuk ke seluruh nama file
+    outfile.open(NamaFIle + ".txt", ios::out);
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
 }
