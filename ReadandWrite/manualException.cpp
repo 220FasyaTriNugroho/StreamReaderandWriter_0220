8066fbf327950744480dc1fe79e108320a82d2ec
220FasyaTriNugroho <fasya.tri.ft24@mail.umy.ac.id>
2025-06-09 18:49:41 +0700

    membuat kondisi catch

--- ReadandWrite/manualException.cpp
+++ ReadandWrite/manualException.cpp
@@ -3,5 +3,23 @@ using namespace std;
 
 int main()
 {
+    try
+    {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        throw("halo"); //melemparkan sebuah integer maha
+        cout << "pernyataan tidak akan dieksekusi" << endl;
+    }
+    catch(int a)
+    {
+        //blok ini akan dieksekusi
+        cout << a << " : Pengecualian ini akan dieksekusi" << endl;
+    }
+    catch (...)
+    {
+        //Jika selain integer maka block ini akan dieksekusi
+        cout << "Default Pengecualian Dieksekusi" << endl;
+    }
+
+    return 0;
     
 }
