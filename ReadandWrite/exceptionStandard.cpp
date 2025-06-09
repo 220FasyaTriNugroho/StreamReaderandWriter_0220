37cf7e3235fe978cff6f4c09aba60a4777d9ad52
220FasyaTriNugroho <fasya.tri.ft24@mail.umy.ac.id>
2025-06-09 19:00:20 +0700

    membuat output penanda bahwa program berjalan

--- ReadandWrite/exceptionStandard.cpp
+++ ReadandWrite/exceptionStandard.cpp
@@ -20,7 +20,7 @@ int main()
         //akan dieksekusi karna array data hanya memiliki 3 elemen
     }
     cout << "Baris Program yang berakhir" << endl;
-
+    //penanda II: bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan
     return 0;
     
 }
