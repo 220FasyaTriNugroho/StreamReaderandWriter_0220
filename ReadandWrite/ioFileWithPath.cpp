163bd28afdf35a2cfcadb74165f3c5484630fadd
220FasyaTriNugroho <fasya.tri.ft24@mail.umy.ac.id>
2025-06-09 18:24:59 +0700

    membuat loop untuk menulis

--- ReadandWrite/ioFileWithPath.cpp
+++ ReadandWrite/ioFileWithPath.cpp
@@ -17,4 +17,13 @@ int main ()
     outfile.open(NamaFIle + ".txt", ios::out);
 
     cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    //unlimited loop untuk menulis
+    while (true)
+    {
+        cout << "- ";
+        //mendapatkan setiap karakter dalam satu bari
+        getline(cin, baris);
+    }
+    
 }
